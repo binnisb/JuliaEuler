64d6061e3a224e500958511b1068d750c28f13dd
Documenter.jl <documenter@juliadocs.github.io>
2021-09-03 08:42:55 +0000

    build based on 0820009

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JuliaEuler","category":"page"},{"location":"#JuliaEuler","page":"Home","title":"JuliaEuler","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JuliaEuler.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JuliaEuler]","category":"page"}]
+}
